ab3d39c5d4ae1687114fced42cfa91265a7a7b38
Chris <chrisjacobson1986@gmail.com>
2017-09-28 11:57:40 -0400

    Created container routes

--- app/app.js
+++ app/app.js
@@ -7,12 +7,23 @@ import '!file-loader?name=[name].[ext]!./manifest.json';
 
 import Home from 'containers/Home';
 import NotFound from 'containers/NotFound';
+import AddJob from 'containers/AddJob';
+import JobDetails from 'containers/JobDetails';
+import Jobs from 'containers/Jobs';
+import Profile from 'containers/Profile';
+import SignUp from 'containers/SignUp';
+import Admin from 'container/Admin';
 
 ReactDOM.render((
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={Home}/>
-
+      <Route path='/AddJob' component={AddJob}/>
+      <Route path='/JobDetails' component={JobDetails}/>
+      <Route path='/Jobs' component={Jobs}/>
+      <Route path='/Profile' component={Profile}/>
+      <Route path='/SignUp' component={SignUp}/>
+      <Route path='/Admin' component={Admin}/>
       <Route path='*' component={NotFound}/>
     </Switch>
   </BrowserRouter>
