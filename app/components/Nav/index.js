3648959f0c77d24066db47e29fdaf3b15d966583
dijahmac <mcdonald.khadijah@gmail.com>
2017-10-03 17:26:59 -0400

    added new component

---
+++ app/components/Nav/index.js
@@ -0,0 +1,24 @@
+/**
+*
+* Nav
+*
+*/
+
+import React from 'react';
+
+import './style.css';
+import './styleM.css';
+
+export default class Nav extends React.PureComponent {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+Nav.contextTypes = {
+  router: React.PropTypes.object
+};
