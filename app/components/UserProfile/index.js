ff6cf2f0ec45028dd2f0d21ec754be5e0350e2a2
Chris <chrisjacobson1986@gmail.com>
2017-10-04 10:24:51 -0400

    Made UserProfile a dialog box of profile page

--- app/components/UserProfile/index.js
+++ app/components/UserProfile/index.js
@@ -11,62 +11,85 @@ import './styleM.css';
 
 export default class UserProfile extends React.PureComponent {
 
+  constructor (props) {
+    super(props);
+    this.state={
+      notification:""
+    }
+  };
 
+  handleUpdateProfile = () => {
+    this.setState({
+      openUserProfile: !this.state.openUserProfile
+    })
+  }
 
 
   render() {
-    return (
-      <div className="profileBox">
-
-
-        <div className="profileUser">
-          <h2> Profile Name
-            <p><input type="text" /></p>
-          </h2>
+    if(this.props.open === true)
+    {
+      return (
+        <div>
+        <div className="fullOverlay" onClick={this.props.onClose}>
         </div>
-
-        <div className="profileImage">Profile Image
-          <p><input type="text" placeholder="User Image"/></p>
-          <img src={require("../../images/businessWeasel.jpg")} className="profileImage"/>
-        </div>
+        <div className="profileBox">
 
 
-        <div className="profileTitle">Title
-          <p><input type="text" placeholder="title"/></p>
-        </div>
-
-        <div className="profileEmail">Email
-          <p><input type="text" placeholder="email"/></p>
-        </div>
-
-        <div className="profileLocation">Location
-          <p><input type="text" placeholder="location" /></p>
-        </div>
-
-        <div className="profileStatus">Status
-          <p><input type="text" placeholder="status"/></p>
-        </div>
-
-        <article>
-          <div className="profileBio">Profile Bio
-          <p><input type="text" placeholder="Profile Bio"/></p>
+          <div className="profileUser">
+            <h2> Profile Name
+              <p><input type="text" /></p>
+            </h2>
           </div>
-        </article>
 
-        <section className="linksBox">
-          <p><a href="https://www.resume.com">My Resume</a></p>
-          <p><a href="https://www.portfolio.com">My Portfolio or Website</a></p>
-          <p><a href="https://www.linkedin.com">My LinkedIn or Social Media</a></p>
-          <p><a href="https://www.github.com">My Github</a></p>
-        </section>
+          <div className="profileImage">Profile Image
+            <p><input type="text" placeholder="User Image"/></p>
+            <img src={require("../../images/businessWeasel.jpg")} className="profileImage"/>
+          </div>
 
 
-        <button className ="submitButton">
-          <input type="button" value="submit"  />
-        </button>
+          <div className="profileTitle">Title
+            <p><input type="text" placeholder="title"/></p>
+          </div>
 
-      </div>
-    );
+          <div className="profileEmail">Email
+            <p><input type="text" placeholder="email"/></p>
+          </div>
+
+          <div className="profileLocation">Location
+            <p><input type="text" placeholder="location" /></p>
+          </div>
+
+          <div className="profileStatus">Status
+            <p><input type="text" placeholder="status"/></p>
+          </div>
+
+          <article>
+            <div className="profileBio">Profile Bio
+            <p><input type="text" placeholder="Profile Bio"/></p>
+            </div>
+          </article>
+
+          <section className="linksBox">
+            <p><a href="https://www.resume.com">My Resume</a></p>
+            <p><a href="https://www.portfolio.com">My Portfolio or Website</a></p>
+            <p><a href="https://www.linkedin.com">My LinkedIn or Social Media</a></p>
+            <p><a href="https://www.github.com">My Github</a></p>
+          </section>
+
+
+          <button className ="submitButton">
+            <input type="button" value="submit"  />
+          </button>
+
+        </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="dialogOverlayHidden">
+        </div>
+      );
+    }
   }
 }
 
