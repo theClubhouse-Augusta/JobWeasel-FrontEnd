03cf966be753b492cddaa87b8d5f376483f02e99
Sarah <smlengsas@gmail.com>
2017-10-05 15:57:41 -0400

    profile display page

--- app/containers/Profile/index.js
+++ app/containers/Profile/index.js
@@ -18,7 +18,17 @@ export default class Profile extends React.PureComponent {
     super(props);
     this.state = {
       openUserProfile:false,
-      user:JSON.parse(sessionStorage.getItem('user'))
+      // user:JSON.parse(sessionStorage.getItem('user'))
+
+      user:{
+        name:" Business Weasel",
+        email:" businessWeasel@JobWeasel.com",
+        location:" None, YA",
+        bio:" All I do is business, business, business, business, business, business, business, business, business, business, business, business, business, business, business, business, business, business, business,  business, business, business, business, business, business, business, business, business, business, business,  business, business, business, business, business, business, business, business, business, business, business, business, business, business, business, business, business... Is this working??",
+
+
+      }
+
     }
   };
 
@@ -29,26 +39,52 @@ export default class Profile extends React.PureComponent {
     }
 
   render() {
-    console.log(this.state.user.id);
+
     return (
       <div className="profileContainer">
         <Helmet title="Profile" meta={[ { name: 'description', content: 'Description of Profile' }]}/>
         <div className="updateProfileButton" onClick={this.handleUpdateProfile}>
+
           <header>Update Profile
           </header>
-          <p>Email:{this.state.user.email}
-          </p>
-          <p>Name:{this.state.user.name}
-          </p>
-          <p>Description:{this.state.user.bio}
-          </p>
-          <p>Created:{this.state.user.created_at}
-          </p>
-          <p>Updated:{this.state.user.updated_at}
-          </p>
+
         </div>
+
+        <section className="profileDisplay">
+
+          <div className="profileName">
+            <p>Name:</p>
+            <h1>{this.state.user.name}
+            </h1>
+          </div>
+
+          <div className="profileEmail">
+            <p>Email:{this.state.user.email}
+            </p>
+          </div>
+
+          <div className="profileLocation">
+            <p>Location:{this.state.user.location}
+            </p>
+          </div>
+
+          <div className="profileBio">
+            <p>Description:{this.state.user.bio}
+            </p>
+          </div>
+
+        </section>
+
+        <section className="linksBox">
+          <p><a href="https://www.resume.com">My Resume</a></p>
+          <p><a href="https://www.portfolio.com">My Portfolio or Website</a></p>
+          <p><a href="https://www.linkedin.com">My LinkedIn or Social Media</a></p>
+          <p><a href="https://www.github.com">My Github</a></p>
+        </section>
+
         <UserProfile open={this.state.openUserProfile} onClose={this.handleUpdateProfile}>
-          </UserProfile>
+        </UserProfile>
+
       </div>
     );
   }
